a9e05ea934b2d589544e33c1654659bf0ea3edae
Retrosol <r3trosol@gmail.com>
2018-05-15 13:24:24 -0500

    Added Sound, Gas, and Fairy Pokemon in Prism

--- mods/prism/pokedex.js
+++ mods/prism/pokedex.js
@@ -25,10 +25,66 @@ exports.BattlePokedex = {
 		inherit: true,
 		types: ["Ghost", "Gas"],
 	},
+	haunter: {
+		inherit: true,
+		types: ["Ghost", "Gas"],
+	},
+	gastly: {
+		inherit: true,
+		types: ["Ghost", "Gas"],
+	},	
+	torkoal: {
+                inherit: true,
+                types: {"Fire", "Gas"],
+	},
+	koffing: {
+                inherit: true,
+                types: {"Poison", "Gas"],
+	},
+	weezing: {
+                inherit: true,
+                types: {"Poison", "Gas"],
+	},		
 	porygonz: {
 		inherit: true,
-		types: ["Sound"],
+		types: {"Sound"],
 	},
+	igglybuff: {
+	        inherit: true,
+		types: {Sound", "Normal"],
+	},	
+	jigglypuff: {
+	        inherit: true,
+		types: {"Sound", "Fairy"],
+	},
+	wigglytuff: {
+	        inherit: true,
+		types: {"Sound", "Fairy"],
+	},
+	whismur: {
+	        inherit: true,
+		types: {"Sound"],
+	},
+	loudred: {
+	        inherit: true,
+		types: {"Sound"],
+	},
+	exploud: {
+	        inherit: true,
+		types: {"Sound"],
+	},
+	chingling: {
+	        inherit: true,
+		types: {"Sound", "Psychic"],
+	},
+	chimecho: {
+	        inherit: true,
+		types: {"Sound", "Psychic"],
+	},		
+	azumarill: {
+		inherit: true,
+		types: ["Fairy", "Water"],
+	},	
 	sylveon: {
 		species: "Sylveon",
 		types: ["Fairy"],
